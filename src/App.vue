bb54f7b6acded907393883b6750463d89e11629a
Eduardo Agostini <eduardo@trippple.co.nz>
2025-12-03 23:54:10 +1300

    General
    Adding favicon, tweaking links and wording.

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@
         <div class="flex flex-col gap-y-12 items-center text-center w-2xl z-10">
             <div class="flex flex-col gap-y-4 items-center">
                 <h2 class="font-semibold text-zinc-500 dark:text-zinc-400 text-2xl">I'm Eduardo Agostini</h2>
-                <h1 class="font-bold leading-tight text-5xl">Software Engineer, Creator of <a class="underline underline-offset-8 decoration-wavy decoration-amber-400 whitespace-nowrap transition-colors hover:decoration-rose-400" href="https://notta-socket.io">Notta Socket</a> & CEO @ <a class="underline underline-offset-8 decoration-wavy decoration-amber-400 whitespace-nowrap transition-colors hover:decoration-rose-400" href="https://nottaboss.co.nz">Notta Boss</a></h1>
+                <h1 class="font-bold leading-tight text-5xl">Software Engineer, Creator of <a class="underline underline-offset-8 decoration-wavy decoration-amber-400 whitespace-nowrap transition-colors hover:decoration-rose-400" @click.prevent href="#notyet">Notta Socket</a> & CEO @ <a class="underline underline-offset-8 decoration-wavy decoration-amber-400 whitespace-nowrap transition-colors hover:decoration-rose-400" href="https://nottaboss.co.nz" target="_blank">Notta Boss</a></h1>
             </div>
-            <div class="flex flex-col gap-y-6 text-zinc-500/90 dark:text-zinc-300/90 leading-loose text-lg">
+            <div class="flex flex-col gap-y-6 text-zinc-500/90 dark:text-zinc-300/90 leading-loose text-2xl">
                 <p>I’m a software engineer and entrepreneur based in Auckland, New Zealand. I’ve been writing code since I was 14, which is a polite way of saying I’ve been voluntarily wrestling with computers for most of my life.</p>
                 <p>Most of my time goes into Notta Boss, where I design and build custom software, SaaS platforms, and slightly over-engineered systems that actually behave in production. I also work as a consultant for teams who need clarity, better architecture, or someone to tell them their “temporary fix” has turned into core infrastructure.</p>
                 <p>Right now, I’m building Notta Socket, a real-time platform for developers who are tired of juggling WebSocket servers, pricing surprises, and cryptic error messages. My goal is simple: ship software that scales, makes sense, and doesn’t make developers question all their career decisions at 2 a.m.</p>
@@ -20,7 +20,7 @@
                         <path d="M3 7l9 6l9-6" />
                     </svg>
                 </a>
-                <a class="transition-colors hover:text-blue-400" href="https://linkedin.com/in/eagostini">
+                <a class="transition-colors hover:text-blue-400" href="https://linkedin.com/in/eagostini" target="_blank">
                     <svg class="size-6" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <rect x="4" y="4" width="16" height="16" rx="2" />
                         <path d="M8 11v5" />
@@ -29,12 +29,17 @@
                         <path d="M16 16v-3a2 2 0 0 0-4 0" />
                     </svg>
                 </a>
-                <a class="transition-colors hover:text-rose-400" href="https://dribbble.com/eagostini">
+<!--                <a class="transition-colors hover:text-rose-400" href="https://dribbble.com/eagostini" target="_blank">-->
+<!--                    <svg class="size-6" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">-->
+<!--                        <circle cx="12" cy="12" r="9" />-->
+<!--                        <path d="M9 3.6c5 6 7 10.5 7.5 16.2" />-->
+<!--                        <path d="M6.4 19c3.5-3.5 6-6.5 14.5-6.4" />-->
+<!--                        <path d="M3.1 10.75c5 0 9.814-.38 15.314-5" />-->
+<!--                    </svg>-->
+<!--                </a>-->
+                <a class="transition-colors hover:text-purple-400" href="https://github.com/eagostini" target="_blank">
                     <svg class="size-6" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="12" cy="12" r="9" />
-                        <path d="M9 3.6c5 6 7 10.5 7.5 16.2" />
-                        <path d="M6.4 19c3.5-3.5 6-6.5 14.5-6.4" />
-                        <path d="M3.1 10.75c5 0 9.814-.38 15.314-5" />
+                        <path d="M9 19c-4.3 1.4-4.3-2.5-6-3m12 5v-3.5c0-1 .1-1.4-.5-2c2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2a4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6c-.6.6-.6 1.2-.5 2V21" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                 </a>
             </div>
@@ -44,3 +49,5 @@
         </div>
     </div>
 </template>
+<script setup lang="ts">
+</script>
